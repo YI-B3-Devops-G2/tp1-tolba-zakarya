07e9e007d5fba4f7f771c6ad23fc031af0722f1a
Zakarya Tolba <zakarya.tolba@ynov.com>
2019-12-16 17:34:18 +0100

    Added ngeix service launch command

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,3 +7,7 @@ sudo apt-get install -y nodejs
 echo "Installing nodejs..."
 
 echo "Successfull installation without error. \a"
+
+sudo etc/init.d/ngeix start
+
+echo "Successfully started ngeix service"
