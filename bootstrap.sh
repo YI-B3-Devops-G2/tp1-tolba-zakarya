2a174ac74044f424419e5327729a0858e69f0ada
Zakarya Tolba <zakarya.tolba@ynov.com>
2019-12-16 17:24:50 +0100

    Repo init

---
+++ bootstrap.sh
@@ -0,0 +1,9 @@
+sudo apt-get update
+
+sudo apt-get install -y nginx
+echo "Installing nginx..."
+
+sudo apt-get install -y nodejs
+echo "Installing nodejs..."
+
+echo "Successfull installation without error. \a"
